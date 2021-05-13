0556d9d908a0d0dc8bf20c1b0d327da169fb96d0
Musa Faal <fallmoussa98xo@gmail.com>
2021-05-13 20:01:30 +0200

    graceful shutdown serveur et arret par commande client

--- client/client.c
+++ client/client.c
@@ -1,104 +1,181 @@
 #include "client.h"
 #define BUFFER_SIZE 100
-void do_or_exit(int result, const char * erreur){
-  if(result < 0){
-    perror(erreur);
-    exit(EXIT_FAILURE);
-  }
-}
 
-void init_sockaddr_in(struct sockaddr_in * serveur, unsigned short port_serveur, char * ip_serveur){
-    serveur->sin_family = AF_INET;
-    serveur->sin_port = htons(port_serveur);
-    inet_aton(ip_serveur,&(serveur->sin_addr));
-}
+int commencer_sauvegarde;
 
-int initialiser_socket(char * ip_serveur, short port_serveur,
-		       struct sockaddr_in * moi, struct sockaddr_in * serveur){  
-  int sock;
-  do_or_exit(sock = socket(AF_INET, SOCK_STREAM, 0), "Erreur lors de la création de socket TCP");
-  init_sockaddr_in(serveur, port_serveur, ip_serveur);
-  
-  //On établit la connexion avec le serveur
-  do_or_exit(connect(sock, (struct sockaddr *)serveur, sizeof(struct sockaddr_in)),
-	     "Erreur lors de la connexion au serveur");
-  
-  int len = sizeof(struct sockaddr_in);
-  // Permet de connaître le port attribué par le système
-  getsockname(sock, (struct sockaddr *)moi, (socklen_t *) &len); 
-  
-  moi->sin_addr = *((struct in_addr*) gethostbyname("localhost")->h_addr_list[0]) ;
-
-  return sock;
-}
-char  tour_partie(char *mot_trouve,int tentatives_restantes)
+void do_or_exit(int result, const char *erreur)
 {
-  char c;
-  printf("****************************Jeu du Pendu****************************\n");
-  printf("Mot:\t%s\n",mot_trouve);
-  printf("Tentatives restantes:\t%d\n",tentatives_restantes);
-  printf("Proposer une lettre : ");
-  scanf(" %c",&c);
-  getchar();//On elimine le retour chariot
-  printf("\n");
-  return c;
+	if (result < 0)
+	{
+		perror(erreur);
+		exit(EXIT_FAILURE);
+	}
 }
 
-int main(int argc , char ** argv)
+void init_sockaddr_in(struct sockaddr_in *serveur, unsigned short port_serveur, char *ip_serveur)
 {
-  struct sockaddr_in moi; 
-  struct sockaddr_in serveur; 
-  short port_serveur = 0;
-  char * ip_serveur;
-  int sock = 0; /* socket de communication */
+	serveur->sin_family = AF_INET;
+	serveur->sin_port = htons(port_serveur);
+	inet_aton(ip_serveur, &(serveur->sin_addr));
+}
 
-    if (argc != 3) {
-      fprintf(stderr,"usage: %s host port_serveur\n", argv[0]);
-      return EXIT_FAILURE;
-    }
-    port_serveur = atoi(argv[2]);
-    ip_serveur = argv[1];
+int initialiser_socket(char *ip_serveur, short port_serveur,
+	struct sockaddr_in *moi, struct sockaddr_in *serveur)
+{
+	int sock;
+	do_or_exit(sock = socket(AF_INET, SOCK_STREAM, 0), "Erreur lors de la création de socket TCP");
+	init_sockaddr_in(serveur, port_serveur, ip_serveur);
 
-    
-    printf("Connexion au serveur: %d\n", port_serveur);
-    sock = initialiser_socket(ip_serveur, port_serveur, &moi, &serveur);
-    char statut_partie[3];
-    do
-    {
-      char mot_trouve [BUFFER_SIZE];
-      char tentatives_restantes[3];
-      int taille = 0;
-      //On recupere l'etat du jeu avant de l'afficher
-      do_or_exit(recv(sock,&taille,sizeof(taille),0),"Erreur Recv");
-      do_or_exit(recv(sock,mot_trouve,taille,0),"Erreur Recv");
-      do_or_exit(recv(sock,&taille,sizeof(taille),0),"Erreur Recv");        
-      do_or_exit(recv(sock,tentatives_restantes,taille,0),"Erreur recv");
-      char c;
-      c = tour_partie(mot_trouve,atoi(tentatives_restantes));
-      // On envoie au serveur le charactere propose
-                    
-      send(sock,&c,sizeof(char),0);
-      
-      //On recupere l'etat de la partie
-      do_or_exit(recv(sock,&taille,sizeof(taille),0),"Erreur Recv");
-      do_or_exit(recv(sock,&statut_partie,taille,0),"Erreur Recv");
-      system("clear");
-    }while(strcmp(statut_partie,"0")==0);
-    
-    //Si la partie est termine
-    if(strcmp(statut_partie,"1")==0 || strcmp(statut_partie,"2")==0)
-    {
-      system("clear");
-      int taille = 0;
-      char mot_cherche [BUFFER_SIZE];
-      do_or_exit(recv(sock,&taille,sizeof(taille),0),"Erreur Recv");
-      do_or_exit(recv(sock,&mot_cherche,taille,0),"Erreur Recv");
-      strcmp(statut_partie,"1")==0 ? printf("Le mot etait : %s \nVous avez gagne !\n",mot_cherche) 
-                                   : printf("Le mot etait : %s \nVous avez perdu !\n",mot_cherche) 
-      ;
-    }
-    
-    close(sock);
-     
-  return EXIT_SUCCESS;
+	//On établit la connexion avec le serveur
+	do_or_exit(connect(sock, (struct sockaddr *) serveur, sizeof(struct sockaddr_in)),
+		"Erreur lors de la connexion au serveur");
+
+	int len = sizeof(struct sockaddr_in);
+	// Permet de connaître le port attribué par le système
+	getsockname(sock, (struct sockaddr *) moi, (socklen_t*) &len);
+
+	moi->sin_addr = *((struct in_addr *) gethostbyname("localhost")->h_addr_list[0]);
+
+	return sock;
+}
+char tour_partie(char *mot_trouve, int tentatives_restantes)
+{
+	char c;
+	printf("****************************Jeu du Pendu****************************\n");
+	printf("    --Taper '*' pour quitter la partie--\n\n");
+	printf("Mot:\t%s\n", mot_trouve);
+	printf("Tentatives restantes:\t%d\n", tentatives_restantes);
+	printf("Proposer une lettre : ");
+	scanf(" %c", &c);
+	getchar();	//On elimine le retour chariot
+	printf("\n");
+	return c;
+}
+
+void sauvegarde(char *mot_trouve, char *mot_cherche, char *tentatives_restantes)
+{
+	FILE *log = fopen(".save.log", "w");
+	fprintf(log, "%s\n", mot_trouve);
+	fprintf(log, "%s\n", mot_cherche);
+	fprintf(log, "%s\n", tentatives_restantes);
+	fclose(log);
+}
+void chargement(int sock_serveur)
+{
+	FILE *log = fopen(".save.log", "r");
+	char c;
+	if (log != NULL)
+	{
+		commencer_sauvegarde = 1;
+		//On signale au serveur qu'une sauvegarde existe
+		c = 'v';
+		send(sock_serveur, &c, sizeof(char), 0);
+		char *buffer;
+		size_t s = 0;
+		//Lecture et envoie du mot trouve
+		getline(&buffer, &s, log);
+		buffer[strcspn(buffer, "\n")] = 0;
+		int taille = strlen(buffer);
+		do_or_exit(send(sock_serveur, &taille, sizeof(taille), 0), "Erreur send");
+		do_or_exit(send(sock_serveur, buffer, taille, 0), "Erreur send");
+
+		//Lecture et envoie du mot cherche
+		getline(&buffer, &s, log);
+		buffer[strcspn(buffer, "\n")] = 0;
+		taille = strlen(buffer);
+		do_or_exit(send(sock_serveur, &taille, sizeof(taille), 0), "Erreur send");
+		do_or_exit(send(sock_serveur, buffer, taille, 0), "Erreur send");
+
+		//Lecture et envoie du nombre de tentatives restantes
+		getline(&buffer, &s, log);
+		buffer[strcspn(buffer, "\n")] = 0;
+		taille = strlen(buffer);
+		do_or_exit(send(sock_serveur, &taille, sizeof(taille), 0), "Erreur send");
+		do_or_exit(send(sock_serveur, buffer, taille, 0), "Erreur send");
+
+		fclose(log);
+	}
+	else
+	{
+		//On signale au serveur qu'il n'y a aucune sauvegarde
+		c = 'f';
+		send(sock_serveur, &c, sizeof(char), 0);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	struct sockaddr_in moi;
+	struct sockaddr_in serveur;
+	short port_serveur = 0;
+	char *ip_serveur;
+	int sock = 0; /*socket de communication */
+	commencer_sauvegarde = 0;
+	if (argc != 3)
+	{
+		fprintf(stderr, "usage: %s host port_serveur\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	port_serveur = atoi(argv[2]);
+	ip_serveur = argv[1];
+
+	printf("Connexion au serveur: %d\n", port_serveur);
+	sock = initialiser_socket(ip_serveur, port_serveur, &moi, &serveur);
+	char statut_partie[3];
+	*statut_partie = 0;
+	char mot_trouve[BUFFER_SIZE];
+	*mot_trouve = 0;
+	char tentatives_restantes[3];
+	*tentatives_restantes = 0;
+
+	//S'il y a une partie existante on la charge
+	chargement(sock);
+
+	do {
+		int taille = 0;
+		//On recupere l'etat du jeu avant de l'afficher
+		do_or_exit(recv(sock, &taille, sizeof(taille), 0), "Erreur Recv");
+		do_or_exit(recv(sock, mot_trouve, taille, 0), "Erreur Recv");
+		do_or_exit(recv(sock, &taille, sizeof(taille), 0), "Erreur Recv");
+		do_or_exit(recv(sock, tentatives_restantes, taille, 0), "Erreur recv");
+		char c;
+		c = tour_partie(mot_trouve, atoi(tentatives_restantes));
+		// On envoie au serveur le charactere propose
+
+		send(sock, &c, sizeof(char), 0);
+
+		//On recupere l'etat de la partie
+		do_or_exit(recv(sock, &taille, sizeof(taille), 0), "Erreur Recv");
+		do_or_exit(recv(sock, &statut_partie, taille, 0), "Erreur Recv");
+		system("clear");
+	} while (strcmp(statut_partie, "0") == 0);
+
+	// On nettoie l'ecran et on recupere la solution du jeu
+	system("clear");
+	int taille = 0;
+	char mot_cherche[BUFFER_SIZE];
+	do_or_exit(recv(sock, &taille, sizeof(taille), 0), "Erreur Recv");
+	do_or_exit(recv(sock, &mot_cherche, taille, 0), "Erreur Recv");
+
+	//Si la partie s'est termine de façon normale on affiche le resultat du jeu
+	if (strcmp(statut_partie, "1") == 0 || strcmp(statut_partie, "2") == 0)
+	{
+		strcmp(statut_partie, "1") == 0 ? printf("Le mot etait : %s \nVous avez gagne !\n", mot_cherche) :
+			printf("Le mot etait : %s \nVous avez perdu !\n", mot_cherche);
+		// On supprime la derniere sauvegarde si on a commencer la partie a partir d'une sauvegarde
+		int k;
+		if (commencer_sauvegarde == 1)
+			if ((k = unlink(".save.log")) != 0)
+				perror("Erreur lors de la suppression du fichier log");
+	}
+	//Si la partie a ete termine par le serveur ou par le client alors on la sauvegarde
+	else
+	{
+		(strcmp(statut_partie, "3") == 0) ? printf("Le serveur a termine la partie\nSauvegarde de la partie en cours..\n") :
+      printf("Arret de la partie \nSauvegarde de la partie en cours..\n");
+		sauvegarde(mot_trouve, mot_cherche, tentatives_restantes);
+	}
+	close(sock);
+
+	return EXIT_SUCCESS;
 }
--- server/server.c
+++ server/server.c
@@ -1,15 +1,15 @@
 #include "server.h"
-
 #define BUFFER_SIZE 100
 #define MAX_THREAD 4
 
-typedef struct partie {
-  char * mot_cherche;
-  char lettres_trouvees[27];
-  int tentative_restantes;
-  int statut_partie;
-  int socket;
-  struct sockaddr_in info_joueur;
+typedef struct partie
+{
+	char *mot_cherche;
+	char lettres_trouvees[27];
+	int tentative_restantes;
+	int statut_partie;
+	int socket;
+	struct sockaddr_in info_joueur;
 }
 Partie;
 
@@ -21,220 +21,286 @@ pthread_mutex_t verrou;
 
 int continuer_serveur;
 
-void do_or_exit(int result, const char * erreur) {
-  if (result < 0) {
-    perror(erreur);
-    exit(EXIT_FAILURE);
-  }
-}
-void init_sockaddr_in(struct sockaddr_in * serveur, unsigned short port_serveur) {
-  serveur -> sin_family = AF_INET;
-  serveur -> sin_port = htons(port_serveur);
-  serveur -> sin_addr.s_addr = htonl(INADDR_ANY);
-}
-
-int initialiser_socket(struct sockaddr_in * serveur, short mon_port) {
-  int sock;
-  init_sockaddr_in(serveur, mon_port);
-  do_or_exit(sock = socket(AF_INET, SOCK_STREAM, 0), "Erreur lors de la création de socket TCP");
-
-  do_or_exit(bind(sock, (struct sockaddr * ) serveur, sizeof(struct sockaddr_in)),
-    "Erreur lors de l'appel à la fonction bind");
-  do_or_exit(listen(sock, 10), "Erreur lors de l'appel à la fonction listen");
-  return sock;
-}
-
-void envoyer_etat_jeu(Partie * p,char * mot_trouve)
+void do_or_exit(int result, const char *erreur)
 {
-  //D'abord le mot avec les parties qu'il a reussi a dechifrer
-  int taille = strlen(mot_trouve);
-  do_or_exit(send(p->socket,&taille,sizeof(taille),0),"Erreur send");
-  do_or_exit(send(p->socket,mot_trouve,taille,0),"Erreur send");
-  //Ensuite le nombres de tentatives restantes
-  char  converti [2]; 
-  sprintf(converti,"%d",p->tentative_restantes);
-  taille = strlen(converti);
-  converti[1]='\0';
-  do_or_exit(send(p->socket,&taille,sizeof(taille),0),"Erreur send");
-  do_or_exit(send(p->socket,converti,taille,0),"Erreur send");        
+	if (result < 0)
+	{
+		perror(erreur);
+		exit(EXIT_FAILURE);
+	}
 }
-void nouvelle_partie(Partie * p) {
-  char * mot_trouve = malloc(strlen(p->mot_cherche)+1);
-  strcpy(mot_trouve,p->mot_cherche);
-  int i=0;
-  printf("Le mot cherche est '%s' | %ld \n",p->mot_cherche,strlen(p->mot_cherche));
-  while(p->mot_cherche[i]!='\0')
-  {
-    mot_trouve[i]='*';
-    i++;
-  }
-  mot_trouve[strlen(p->mot_cherche)] = '\0';
-  printf("Partie en cours avec le client : %s \n",inet_ntoa(p->info_joueur.sin_addr));
-  while(p->statut_partie == 0 && continuer_serveur)
-  {
-      char c;
-      //on envoie l'etat du jeu au client
-      envoyer_etat_jeu(p,mot_trouve);
-      // On recupere le charactere propose par le client
-      do_or_exit(recv(p->socket,&c,sizeof(char),0),"Erreur Recv" );
-      
-      // Si la lettre figure dans le mot et n'a pas precedemment ete propose on l'ajoute aux lettre trouvees
-      if(strchr(p->mot_cherche,c)!=NULL && strchr(p->lettres_trouvees,c)==NULL)
-      {
-        char concat[2];
-        concat[0] = c;
-        concat[1] = '\0';
-        strcat(p->lettres_trouvees,concat);
-      }
-      else 
-        p->tentative_restantes --;
-
-      
-      if(strchr(p->mot_cherche,c)!=NULL)
-        printf("Lettre %c dans mot \n",c);
-      printf("Lettres trouvees : %s\n",p->lettres_trouvees);
-      
-      
-      //On met a jour la variable mot trouve
-      int k = 0;
-      while(p->mot_cherche[k]!='\0')
-      {
-      if(strchr(p->lettres_trouvees,p->mot_cherche[k])!=NULL)
-        mot_trouve[k]= p->mot_cherche[k];
-      k++;
-      }
-      
-      // Si le mot est trouve alors la partie prend fin
-      if(strcmp(p->mot_cherche,mot_trouve) == 0)
-        p->statut_partie = 1;
-      else if(p->tentative_restantes == 0)
-        p->statut_partie = 2;
-      // sinon avant de continuer la partie on verifie que l'arret du serveur n'a pas ete demande
-      // dans ce cas on signale au client de sauvegarder la partie (statut 3)
-      else if (continuer_serveur == 0)
-        p->statut_partie = 3;
-
-      //Envoie du statut de la partie au client
-      char  converti [3];
-      sprintf(converti,"%d",p->statut_partie);
-      int taille = strlen(converti);
-      do_or_exit(send(p->socket,&taille,sizeof(taille),0),"Erreur send");
-      do_or_exit(send(p->socket,converti,taille,0),"Erreur send");
-  }
-  // Si la partie est termine on envoie la solution
-  if(p->statut_partie == 1 || p->statut_partie == 2)
-      {
-      int taille = strlen(p->mot_cherche);
-      do_or_exit(send(p->socket,&taille,sizeof(taille),0),"Erreur send");
-      do_or_exit(send(p->socket,p->mot_cherche,taille,0),"Erreur send");  
-      }
-   
-}
-
-void * creer_th(void * arg) {
-  char buff[BUFFER_SIZE];
-  Partie * p = (Partie * ) arg;
-  int sock = p -> socket;
-  nouvelle_partie(p);
-  close(sock);
-  printf("Partie terminee avec le client : %s \n",inet_ntoa(p->info_joueur.sin_addr));
-  pthread_mutex_lock(&verrou);
-  NB_PARTIE--;
-  pthread_mutex_unlock(&verrou);
-  free(p->mot_cherche);
-  free(arg);
-}
-
-Partie * init_partie(int sock) {
-  Partie * p;
-  p = malloc(sizeof(Partie));
-  p -> lettres_trouvees [0] = '\0';
-  p -> statut_partie = 0;
-  p -> tentative_restantes = 8;
-  p -> socket = sock;
-  // On intialise le mot a trouver pour cette partie
-  srand(time(0));
-  int num = (rand() % (30 - 1 + 1)) + 1;
-  FILE * f = fopen("server/ressources/mots.txt", "r");
-  if (f == NULL) {
-    printf("Erreur ouverture fichier\n");
-    exit(EXIT_FAILURE);
-  }
-  char ligne[256];
-  int i = 0;
-  while (fgets(ligne, sizeof(ligne), f)) {
-    i++;
-    if (i == num) 
-    {
-      p -> mot_cherche = malloc(strlen(ligne)+1);
-      strcpy(p->mot_cherche, ligne);
-      p->mot_cherche[strcspn(p->mot_cherche, "\n")] = 0; //On enleves le retour chariot
-      break;
-    }
-  }
-  fclose(f);
-  return p;
-}
-
-void  set_continuer(int signal)
+void init_sockaddr_in(struct sockaddr_in *serveur, unsigned short port_serveur)
 {
-  continuer_serveur = 0;  
+	serveur->sin_family = AF_INET;
+	serveur->sin_port = htons(port_serveur);
+	serveur->sin_addr.s_addr = htonl(INADDR_ANY);
 }
 
-int main(int argc, char ** argv) {
-  struct sockaddr_in serveur;
-  struct sockaddr_in client;
-  char message[BUFFER_SIZE];
-  int taille_serveur = sizeof(serveur);
-  short port = 0;
-  char buffer[255];
-  sock_ecoute = 0;
-  continuer_serveur = 1;
-  
-  struct sigaction s;
-  s.sa_handler = set_continuer;
-  sigaction(SIGINT, &s,0);
-  
-  pthread_t ths[MAX_THREAD];
-  pthread_mutex_init( & verrou, NULL);
-  NB_PARTIE = 0;
-  if (argc != 2) {
-    fprintf(stderr, "usage: %s port\n", argv[0]);
-    return EXIT_FAILURE;
-  }
-  port = atoi(argv[1]);
-  sock_ecoute = initialiser_socket( & serveur, port);
+int initialiser_socket(struct sockaddr_in *serveur, short mon_port)
+{
+	int sock;
+	init_sockaddr_in(serveur, mon_port);
+	do_or_exit(sock = socket(AF_INET, SOCK_STREAM, 0), "Erreur lors de la création de socket TCP");
 
-  int sock_comm;
-  //Le serveur ecoute et attend de nouvelles connexion
-  do_or_exit(listen(sock_ecoute, 30), "Erreur listen");
-  printf("En attente de connexion\n");
+	do_or_exit(bind(sock, (struct sockaddr *) serveur, sizeof(struct sockaddr_in)),
+		"Erreur lors de l'appel à la fonction bind");
+	do_or_exit(listen(sock, 10), "Erreur lors de l'appel à la fonction listen");
+	return sock;
+}
 
-  while (continuer_serveur) {
-    if(NB_PARTIE<MAX_THREAD)
+void envoyer_etat_jeu(Partie *p, char *mot_trouve)
+{
+	//D'abord le mot avec les parties qu'il a reussi a dechifrer
+	int taille = strlen(mot_trouve);
+	do_or_exit(send(p->socket, &taille, sizeof(taille), 0), "Erreur send");
+	do_or_exit(send(p->socket, mot_trouve, taille, 0), "Erreur send");
+	//Ensuite le nombres de tentatives restantes
+	char converti[2];
+	sprintf(converti, "%d", p->tentative_restantes);
+	taille = strlen(converti);
+	converti[1] = '\0';
+	do_or_exit(send(p->socket, &taille, sizeof(taille), 0), "Erreur send");
+	do_or_exit(send(p->socket, converti, taille, 0), "Erreur send");
+}
+
+char *charger_partie(Partie *p, int *tentatives_restantes)
+{
+	char c;
+	char *mot_trouve;
+	do_or_exit(recv(p->socket, &c, sizeof(char), 0), "Erreur Recv");
+	if (c == 'v')
+	{
+		char buff[BUFFER_SIZE];
+		int taille;
+		//On recupere le mot trouve
+		do_or_exit(recv(p->socket, &taille, sizeof(taille), 0), "Erreur Recv");
+		do_or_exit(recv(p->socket, buff, taille, 0), "Erreur Recv");
+		mot_trouve = malloc(strlen(buff) + 1);
+		strcpy(mot_trouve, buff);
+		mot_trouve[strlen(buff)] = '\0';
+		//On recupere le mot cherche
+		do_or_exit(recv(p->socket, &taille, sizeof(taille), 0), "Erreur Recv");
+		do_or_exit(recv(p->socket, buff, taille, 0), "Erreur Recv");
+		p->mot_cherche = malloc(strlen(buff) + 1);
+		strcpy(p->mot_cherche, buff);
+		//On recupere le nombre de tentatives restantes
+		do_or_exit(recv(p->socket, &taille, sizeof(taille), 0), "Erreur Recv");
+		do_or_exit(recv(p->socket, buff, taille, 0), "Erreur Recv");
+		*tentatives_restantes = atoi(buff);
+		return mot_trouve;
+	}
+	return NULL;
+}
+
+void nouvelle_partie(Partie *p)
+{
+	char *mot_trouve = NULL;
+	//S'il n'y a aucune sauvegarde on demarre la partie avec la config intiale
+	if ((mot_trouve = charger_partie(p, &p->tentative_restantes)) == NULL)
+	{
+		mot_trouve = malloc(strlen(p->mot_cherche) + 1);
+		strcpy(mot_trouve, p->mot_cherche);
+		int i = 0;
+		while (p->mot_cherche[i] != '\0')
+		{
+			mot_trouve[i] = '*';
+			i++;
+		}
+		mot_trouve[strlen(p->mot_cherche)] = '\0';
+	}
+	else
+	// Si la partie demarre a partie d'une sauvegarde alors on met a jour la variable letrres trouvees 
+	{
+		int i = 0;
+		while (mot_trouve[i] != '\0')
+		{
+			if (strchr(p->lettres_trouvees, mot_trouve[i]) == NULL && mot_trouve[i] != '*')
+			{
+				char concat[2];
+				concat[0] = mot_trouve[i];
+				concat[1] = '\0';
+				strcat(p->lettres_trouvees, concat);
+			}
+			i++;
+		}
+	}
+	printf("Partie en cours avec le client : %s le mot cherche est %s\n", inet_ntoa(p->info_joueur.sin_addr), p->mot_cherche);
+
+	while (p->statut_partie == 0 && continuer_serveur)
+	{
+		char c;
+		//on envoie l'etat du jeu au client
+		envoyer_etat_jeu(p, mot_trouve);
+		// On recupere le charactere propose par le client
+		do_or_exit(recv(p->socket, &c, sizeof(char), 0), "Erreur Recv");
+
+		// Si la lettre figure dans le mot et n'a pas precedemment ete propose on l'ajoute aux lettre trouvees
+		if (strchr(p->mot_cherche, c) != NULL && strchr(p->lettres_trouvees, c) == NULL)
+		{
+			char concat[2];
+			concat[0] = c;
+			concat[1] = '\0';
+			strcat(p->lettres_trouvees, concat);
+		}
+		else
+			p->tentative_restantes--;
+
+		//On met a jour la variable mot trouve
+		int k = 0;
+		while (p->mot_cherche[k] != '\0')
+		{
+			if (strchr(p->lettres_trouvees, p->mot_cherche[k]) != NULL)
+				mot_trouve[k] = p->mot_cherche[k];
+			k++;
+		}
+
+		// Si le mot est trouve alors la partie prend fin
+		if (strcmp(p->mot_cherche, mot_trouve) == 0)
+			p->statut_partie = 1;
+		else if (p->tentative_restantes == 0)
+			p->statut_partie = 2;
+      
+		// sinon avant de continuer la partie on verifie que l'arret du serveur n'a pas ete demande
+		
+    // dans ce cas on signale au client de sauvegarder la partie (statut 3)
+		else if (continuer_serveur == 0)
+			p->statut_partie = 3;
+    
+    // Cas ou le client demande l'arret de la partie
+    else if(c == '*')
     {
-    sock_comm = accept(sock_ecoute, (struct sockaddr * ) & client, (socklen_t * ) & taille_serveur);
-    if(sock_comm!=-1)
-    {
-    printf("Connexion acceptée: (%s,%4d)\n", inet_ntoa(serveur.sin_addr), ntohs(serveur.sin_port));
-    Partie * p = init_partie(sock_comm);
-    pthread_create(&ths[NB_PARTIE], NULL, creer_th, p);
-    NB_PARTIE++;
-    sleep(5);
+      p->statut_partie = 4;
+      p->tentative_restantes++;
     }
-    }
-    else 
-    {
-    printf("Serveur occupee, %d partie deja en cours ..\n",NB_PARTIE);
-    sleep(5);
-    }    
-  }
-  close(sock_ecoute);
-  printf("Sauvegrade des parties en cours et arret du serveur ..\n");
-  int i;
-  for (i = 0; i < NB_PARTIE; ++i) 
-        pthread_join(ths[i], NULL);
-  
-  pthread_mutex_destroy(&verrou);
-  return EXIT_SUCCESS;
+      
+
+		//Envoie du statut de la partie au client
+		char converti[3];
+		sprintf(converti, "%d", p->statut_partie);
+		int taille = strlen(converti);
+		do_or_exit(send(p->socket, &taille, sizeof(taille), 0), "Erreur send");
+		do_or_exit(send(p->socket, converti, taille, 0), "Erreur send");
+	}
+	// Si la partie est termine ou si l'aret du serveur est demande on envoie la solution
+	if (p->statut_partie != 0)
+	{
+		int taille = strlen(p->mot_cherche);
+		do_or_exit(send(p->socket, &taille, sizeof(taille), 0), "Erreur send");
+		do_or_exit(send(p->socket, p->mot_cherche, taille, 0), "Erreur send");
+	}
+}
+
+void *creer_th(void *arg)
+{
+	char buff[BUFFER_SIZE];
+	Partie *p = (Partie*) arg;
+	int sock = p->socket;
+	nouvelle_partie(p);
+	close(sock);
+	printf("Partie terminee avec le client : %s \n", inet_ntoa(p->info_joueur.sin_addr));
+	pthread_mutex_lock(&verrou);
+	NB_PARTIE--;
+	pthread_mutex_unlock(&verrou);
+	free(p->mot_cherche);
+	free(arg);
+}
+
+Partie* init_partie(int sock)
+{
+	Partie * p;
+	p = malloc(sizeof(Partie));
+	p->lettres_trouvees[0] = '\0';
+	p->statut_partie = 0;
+	p->tentative_restantes = 8;
+	p->socket = sock;
+	// On intialise le mot a trouver pour cette partie
+	srand(time(0));
+	int num = (rand() % (30 - 1 + 1)) + 1;
+	FILE *f = fopen("server/ressources/mots.txt", "r");
+	if (f == NULL)
+	{
+		printf("Erreur ouverture fichier\n");
+		exit(EXIT_FAILURE);
+	}
+	char ligne[256];
+	int i = 0;
+	while (fgets(ligne, sizeof(ligne), f))
+	{
+		i++;
+		if (i == num)
+		{
+			p->mot_cherche = malloc(strlen(ligne) + 1);
+			strcpy(p->mot_cherche, ligne);
+			p->mot_cherche[strcspn(p->mot_cherche, "\n")] = 0;	//On enleves le retour chariot
+			break;
+		}
+	}
+	fclose(f);
+	return p;
+}
+
+void set_continuer(int signal)
+{
+	continuer_serveur = 0;
+}
+
+int main(int argc, char **argv)
+{
+	struct sockaddr_in serveur;
+	struct sockaddr_in client;
+	char message[BUFFER_SIZE];
+	int taille_serveur = sizeof(serveur);
+	short port = 0;
+	char buffer[255];
+	sock_ecoute = 0;
+	continuer_serveur = 1;
+
+	struct sigaction s;
+	s.sa_handler = set_continuer;
+	sigaction(SIGINT, &s, 0);
+
+	pthread_t ths[MAX_THREAD];
+	pthread_mutex_init(&verrou, NULL);
+	NB_PARTIE = 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "usage: %s port\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+	port = atoi(argv[1]);
+	sock_ecoute = initialiser_socket(&serveur, port);
+
+	int sock_comm;
+	//Le serveur ecoute et attend de nouvelles connexion
+	do_or_exit(listen(sock_ecoute, 30), "Erreur listen");
+	printf("En attente de connexion\n");
+
+	while (continuer_serveur)
+	{
+		if (NB_PARTIE < MAX_THREAD)
+		{
+			sock_comm = accept(sock_ecoute, (struct sockaddr *) &client, (socklen_t*) &taille_serveur);
+			if (sock_comm != -1)
+			{
+				printf("Connexion acceptée: (%s,%4d)\n", inet_ntoa(serveur.sin_addr), ntohs(serveur.sin_port));
+				Partie *p = init_partie(sock_comm);
+				pthread_create(&ths[NB_PARTIE], NULL, creer_th, p);
+				NB_PARTIE++;
+				sleep(5);
+			}
+		}
+		else
+		{
+			printf("Serveur occupee, %d partie deja en cours ..\n", NB_PARTIE);
+			sleep(5);
+		}
+	}
+	close(sock_ecoute);
+	printf("Sauvegrade des parties en cours et arret du serveur ..\n");
+	int i;
+	for (i = 0; i < NB_PARTIE; ++i)
+		pthread_join(ths[i], NULL);
+
+	pthread_mutex_destroy(&verrou);
+	return EXIT_SUCCESS;
 }
